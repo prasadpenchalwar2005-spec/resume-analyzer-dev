65f987eb55e7060786a4d646049dc33dfb71a03f
prasadpenchalwar2005-spec <prasadpenchalwar2005@gmail.com>
2026-09-14 12:35:23 +0530

    Use stable Vercel domain for CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "https://resume-analyzer-mff72cfud-prasad-f8cb.vercel.app",
+    origin:  "https://resume-analyzer-dev.vercel.app",
     credentials: true
 }))
 
