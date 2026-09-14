e4d8787d40c0fc50006e097f45640a4483302fe4
prasadpenchalwar2005-spec <prasadpenchalwar2005@gmail.com>
2026-09-14 07:58:36 +0530

    Update server port for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,8 @@ const connectDB = require("./src/config/database")
 
 connectDB()
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
 })
